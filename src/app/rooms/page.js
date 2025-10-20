ada83fcd96fb6e76ddf4d3ac52f8651655d93b97
TinaTech-Developers <tinashephiri0@gmail.com>
2025-10-20 13:23:35 +0200

    accomodationPage error

--- src/app/rooms/page.js
+++ src/app/rooms/page.js
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 import MiniHeader from "./_components/MiniHeader";
 import ApartmentAvailability from "./_components/ApartmentAvailability";
 import ListOfAvailableApartments from "./_components/ListOfAvailableApartments";
-import LuxuryExperiencePage from "./_components/AccommodationExperiencePage(";
+import LuxuryExperiencePage from "./_components/AccommodationExperiencePage";
 
 function Rooms() {
   const [availableRooms, setAvailableRooms] = useState([]);
